55cb5d7eb7631caeb9c1f7a897b446671ecd686e
macbook <macbook@macbookdemacbook-air.local>
2018-09-08 18:02:31 +0800

    创建cell

---
+++ TieJunLing/我的钱包/cell/LHDetailTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LHDetailTableViewCell.h
+//  TieJunLing
+//
+//  Created by macbook on 2018/9/8.
+//  Copyright © 2018年 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHDetailTableViewCell : UITableViewCell
+
+@end
---
+++ TieJunLing/我的钱包/cell/LHDetailTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  LHDetailTableViewCell.m
+//  TieJunLing
+//
+//  Created by macbook on 2018/9/8.
+//  Copyright © 2018年 macbook. All rights reserved.
+//
+
+#import "LHDetailTableViewCell.h"
+
+@implementation LHDetailTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
